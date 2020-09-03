22d068c2a38d2766280ed3616483c2299b477173
Sitasrsw <70082171+Sitasrsw@users.noreply.github.com>
2020-09-03 12:53:18 +0700

    Menghitung Luas Persegi Panjang

---
+++ Menghitung_luas_persegi_panjang.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mengitungluas;
+
+/**
+ *
+ * @author sita saraswati
+ */
+public class Menghitung_luas_persegi_panjang {
+ public static void main(String[] args) {
+     double p=15, l=6, luas;
+     //*perhitungan
+     luas=p*l;
+     //*output
+     System.out.println("===PROGRAM MENGHITUNG LUAS PERSEGI PANJANG===");
+     System.out.println("Panjang\t= "+p+"cm");
+     System.out.println("Lebar\t= "+l+"cm");
+     System.out.println("Luas\t= PxL");
+     System.out.println("\t= "+luas+"cm2");
+     
+ }   
+}
