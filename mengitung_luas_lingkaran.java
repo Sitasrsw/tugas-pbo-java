d572bebafc146b7ed9ba2af9130b71ccb841a690
Sitasrsw <70082171+Sitasrsw@users.noreply.github.com>
2020-09-03 12:52:35 +0700

    Menghitung Luas Lingkaran

---
+++ mengitung_luas_lingkaran.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mengitungluas;
+
+/**
+ *
+ * @author Sita Saraswati
+ */
+public class mengitung_luas_lingkaran {
+ public static void main(String[] args) {
+     double phi = 3.14;
+     int r = 7;
+     double L = phi * (r * r);
+     
+     System.out.println("===PROGRAM MENGHITUNG LUAS LINGKARAN===");
+     System.out.println("phi\t= "+phi+"cm");
+     System.out.println("r\t= "+r+"cm");
+     System.out.println("L\t= phi*(r*r)");
+     System.out.println("\t= "+L+"cm2");
+     
+    }
+ }
