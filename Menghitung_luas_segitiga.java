598c7d6add1383c360a63c96079b4bd0e69f11af
Sitasrsw <70082171+Sitasrsw@users.noreply.github.com>
2020-09-03 12:51:50 +0700

    Menghitung Luas Segitiga

---
+++ Menghitung_luas_segitiga.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mengitungluas;
+
+/**
+ *
+ * @author Sita Saraswati
+ */
+public class Menghitung_luas_segitiga {
+ public static void main(String[] args) {
+         double a=16, t=20, luas;
+     //*perhitungan
+        luas= a*t/2;
+     //*output
+     System.out.println("===PROGRAM MENGHITUNG LUAS SEGITIGA===");
+     System.out.println("Alas\t= "+a+"cm");
+     System.out.println("Tinggi\t= "+t+"cm");
+     System.out.println("Luas\t= AxT/2");
+     System.out.println("\t= "+luas+"cm2");
+
+    }
+}
