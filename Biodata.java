54e7c527cf060f11b510c68810b7778d9f689e3f
Sitasrsw <70082171+Sitasrsw@users.noreply.github.com>
2020-10-16 11:09:22 +0700

    Script biodata dalam class biodata

---
+++ Biodata.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Biodata {
+    public void biodataNama(){
+        System.out.println("Nama : Sita Saraswati");
+    }
+    
+    public void biodataKelas(){
+        System.out.println("Kelas : XI");
+    }
+    
+    public void biodataJurusan(){
+        System.out.println("Jurusan : Rekayasa Perangkat Lunak 1");
+    }
+    
+    public void biodataAbsen(){
+        System.out.println("No absen : 30");
+    }
+    
+    public void biodataAlamat(){
+        System.out.println("Alamat : Dsn.Genengan, Ds.Glagahsari, Kec.Sukorejo, Kab.Pasuruan");
+    }
+    
+    public void biodataTTL(){
+        System.out.println("TTL : Pasuruan, 12 Juni 2003");
+    }
+    
+    public void biodataUmur(){
+        System.out.println("Umur : 17 Tahun");
+    }
+    
+    public void biodataHobi(){
+        System.out.println("Hobi : Menyanyi, Membaca, Mendengarkan Musik, Rebahan");
+    }
+    
+  }
+
