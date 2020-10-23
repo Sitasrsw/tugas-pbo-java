bcbeb89fab31f9d8d2d4b0767a2c5d08d1e56996
Sitasrsw <70082171+Sitasrsw@users.noreply.github.com>
2020-10-23 12:35:57 +0700

    Script calculator

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Calculator {
+    
+    public double getPenjumlahan(int a, int b){
+        double hasil = a + b;
+        return hasil;   
+    }
+    
+    public int getPerkalian(int c, int d){
+        int perkalian = c * d;
+        return perkalian;
+    }
+    
+    public int getPengurangan(int e, int f){
+        int pengurangan = e - f;
+        return pengurangan;
+    }
+    
+    public int getPembagian(int g, int h){
+        int pembagian = g/h;
+        return pembagian;
+    }
+}
